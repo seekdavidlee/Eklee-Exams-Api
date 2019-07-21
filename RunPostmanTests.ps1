62f7746a73dafea6030468632f3aab3052a5013c
David Lee <seekdavidlee@gmail.com>
2019-07-20 21:08:37 -0500

    Use generated code.

--- RunPostmanTests.ps1
+++ RunPostmanTests.ps1
@@ -1,43 +1,28 @@
-$Tenant1Id = $env:tenant1Id
-$Tenant1user1 = $env:tenant1user1
-$Tenant1user1password = $env:tenant1user1password
-$ResourceId = $env:resourceId
-$ClientId = $env:clientId 
-$ClientSecret = $env:clientSecret
 $StackName = $env:stackName
 	
-	  $url = "https://" + $StackName + "-staging.azurewebsites.net"
-      @{
-        values=@(
-        @{
-            key="endpoint";
-            value="$url";
-        }, 
-        @{
-            key="tenant1Id";
-            value="$Tenant1Id";
-        }, 
-        @{
-            key="clientId";
-            value="$ClientId";
-        }, 
-        @{
-            key="clientSecret";
-            value="$ClientSecret";
-        }, 
-        @{
-            key="tenant1user1";
-            value="$Tenant1user1";
-        }, 
-        @{
-            key="tenant1user1password";
-            value="$Tenant1user1password";
-        }, 
-        @{
-            key="resourceId";
-            value="$ResourceId";
-        })
-      } | ConvertTo-Json -depth 100 | Out-File -encoding ASCII postman_environment.json
-      npm install
-      Write-Host "Running postman tests."
-	  node_modules\.bin\newman run tests\Eklee.Exam.Api.postman_collection.json -e postman_environment.json --reporters junit --reporter-junit-export $env:Common_TestResultsDirectory\report.xml
+$url = "https://" + $StackName + "-staging.azurewebsites.net"
+@{
+    values = @(
+        @{ key = "endpoint"; value = "$env:Postman_endpoint"; },
+        @{ key = "client_secret"; value = "$env:Postman_client_secret"; },
+        @{ key = "client_id"; value = "$env:Postman_client_id"; },
+        @{ key = "resource_id"; value = "$env:Postman_resource_id"; },
+        @{ key = "tenant_id"; value = "$env:Postman_tenant_id"; },
+        @{ key = "isRemote"; value = "$env:Postman_isRemote"; },
+        @{ key = "tenant1Id"; value = "$env:Postman_tenant1Id"; },
+        @{ key = "tenant1user1"; value = "$env:Postman_tenant1user1"; },
+        @{ key = "tenant1user1password"; value = "$env:Postman_tenant1user1password"; },
+        @{ key = "resourceId"; value = "$env:Postman_resourceId"; },
+        @{ key = "clientId"; value = "$env:Postman_clientId"; },
+        @{ key = "clientSecret"; value = "$env:Postman_clientSecret"; },
+        @{ key = "adminUser1"; value = "$env:Postman_adminUser1"; },
+        @{ key = "adminUser1password"; value = "$env:Postman_adminUser1password"; },
+        @{ key = "adminResourceId"; value = "$env:Postman_adminResourceId"; },
+        @{ key = "adminId"; value = "$env:Postman_adminId"; },
+        @{ key = "adminClientId"; value = "$env:Postman_adminClientId"; },
+        @{ key = "adminClientSecret"; value = "$env:Postman_adminClientSecret"; },
+    )
+} | ConvertTo-Json -depth 100 | Out-File -encoding ASCII postman_environment.json
+npm install
+Write-Host "Running postman tests."
+node_modules\.bin\newman run tests\Eklee.Exam.Api.postman_collection.json -e postman_environment.json --reporters junit --reporter-junit-export $env:Common_TestResultsDirectory\report.xml
--- Util/LocalSettingsTOARM.ps1
+++ Util/LocalSettingsTOARM.ps1
@@ -1,19 +1,16 @@
 param(
     [string]$LocalSettingsFilePath, 
+    [string]$LocalPostmanEnvironmentFilePath, 
     [string]$armTemplate,
     [string]$organization,
     [string]$project,
     [string]$username,
     [securestring]$password)
 
-$content = Get-Content -Path $LocalSettingsFilePath | ConvertFrom-Json
-
-$global:gl = @()
-$global:pl = @()
-
-$ignoreKeywords = @("Values:", "IsEncrypted", "Host:")
 function ProcessValue {
     param ($p, $key)
+    
+    $ignoreKeywords = @("Values:", "IsEncrypted", "Host:")
 
     $any = $ignoreKeywords | Where-Object { $key.StartsWith($_) }
 
@@ -59,7 +56,6 @@ function ReadObj {
     foreach ($p in $obj.PsObject.Properties) {
 
         $propertyName = $p.Name
-        Write-Host "Assert" $propertyName -ForegroundColor Green
 
         if ($p.Value -is [array]) {
             
@@ -107,27 +103,92 @@ function ReadObj {
     }
 }
 
-ReadObj -obj $content -key $null
+function UpdateDevOpsLibrary {
+    param (
+        $organization,
+        $project,
+        $variableGroupName,
+        $variables,
+        $username,
+        [SecureString]$password
+    )
 
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+    $uPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+    $bytes = [System.Text.ASCIIEncoding]::ASCII.GetBytes("${username}:${uPassword}")
+    $encodedText = [Convert]::ToBase64String($bytes)
+
+    $url = "https://dev.azure.com/$organization/$project/_apis/distributedtask/variablegroups?api-version=5.0-preview.1"
+
+    $autHeaders = @{"Authorization" = "Basic $encodedText"; }
+    $payloads = Invoke-RestMethod -Method Get -Uri $url -Headers $autHeaders -ContentType "application/json"
+
+    $updateSettings = $null
+    $payloads.Value | ForEach-Object {
+        if ($_.Name -eq $variableGroupName) {
+            $updateSettings = $_        
+        }
+    }
+    if ($null -eq $updateSettings) {
+        return
+    }
+
+    $id = $updateSettings.Id
+    $url = "https://dev.azure.com/$organization/$project/_apis/distributedtask/variablegroups/" + $id + "?api-version=5.0-preview.1"
+    $obj = @{
+        "Type"        = $updateSettings.Type;
+        "Name"        = $updateSettings.Name;
+        "Description" = "Updated"
+        "Variables"   = $variables
+    }
+
+    $body = ($obj | ConvertTo-Json -depth 50).Replace("\u0027", "'")
+    Invoke-RestMethod -Method Put -Uri $url -Headers $autHeaders -ContentType "application/json" -Body $body -ErrorAction Stop
+}
+
+$postmanContent = Get-Content -Path $LocalPostmanEnvironmentFilePath | ConvertFrom-Json
+
+$variables = New-Object -TypeName psobject
+
+$postmanBody = ""
+$envOutput = ""
+$postmanContent.Values | ForEach-Object {
+    $name = $_.Key
+    $parameterName = "Postman." + $name.Replace(":", "_")
+    $isSecret = ($parameterName.ToLower().EndsWith("secret") -or $parameterName.ToLower().EndsWith("password")).ToString()
+    $variables | Add-Member -MemberType NoteProperty -Name $parameterName -Value @{"Value" = $_.Value; "IsSecret" = $isSecret; } -ErrorAction Stop
+    $envName = "Postman_" + $name.Replace(":", "_")
+
+    if ($isSecret -eq "True") {
+        $envOutput += "$name`: `$($envName)`n"
+    }   
+    $postmanBody += "@{ key=""$name""; value=""`$env:$envName""; },`n"
+}
+
+Write-Host $envOutput
+
+Write-Host $postmanBody
+
+UpdateDevOpsLibrary -organization $organization -project $project `
+    -variableGroupName "Postman" `
+    -variables $variables `
+    -username $username `
+    -password $password
+
+$content = Get-Content -Path $LocalSettingsFilePath | ConvertFrom-Json
+
+$global:gl = @()
+$global:pl = @()
+
+ReadObj -obj $content -key $null
 
 $content = Get-Content -Path $armTemplate | ConvertFrom-Json
 
 $appSettings = [PSCustomObject] @{ }
 
 $global:gl | ForEach-Object {
-    $_.Name
-    $_.Value
     $appSettings | Add-Member -MemberType NoteProperty -Name $_.Name -Value $_.Value
-}
-
-if (!$content.variables.appSettings) {    
-    $content.variables | Add-Member -MemberType NoteProperty -Name "appSettings" -Value $appSettings
-    Write-Host "AppSettings property is added." -ForegroundColor Green
-}
-else {
-    Write-Host "AppSettings is there."
-    $content.variables.appSettings = $appSettings
-    Write-Host "AppSettings property is updated." -ForegroundColor Green
 }
 
 $yamlOut = ""
@@ -141,12 +202,12 @@ $content.resources | ForEach-Object {
     if ($_.type -eq "Microsoft.Web/sites/config") {
 
         if (!$_.properties.numberOfWorkers) {
-            $_.properties = "[variables('appSettings')]"
+            $_.properties = $appSettings
         }        
     }
 
     if ($_.type -eq "Microsoft.Web/sites/slots/config") {
-        $_.properties = "[variables('appSettings')]"
+        $_.properties = $appSettings 
     }
 }
 
@@ -154,48 +215,18 @@ $content.resources | ForEach-Object {
 
 Write-Host $yamlOut
 
-$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
-$uPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+$variables = New-Object -TypeName psobject
 
-$bytes = [System.Text.ASCIIEncoding]::ASCII.GetBytes("${username}:${uPassword}")
-$encodedText = [Convert]::ToBase64String($bytes)
-
-$url = "https://dev.azure.com/$organization/$project/_apis/distributedtask/variablegroups?api-version=5.0-preview.1"
-
-$autHeaders = @{"Authorization" = "Basic $encodedText"; }
-$payloads = Invoke-RestMethod -Method Get -Uri $url -Headers $autHeaders -ContentType "application/json"
-
-$updateSettings = $null
-$payloads.Value | ForEach-Object {
-    if ($_.Name -eq "Deploy") {
-        $updateSettings = $_        
-    }
+$global:gl | ForEach-Object {
+    if ($_.AsDeployParameter) {
+        $parameterName = "Deploy." + $_.Name.Replace(":", "_")
+        $isSecret = ($parameterName.EndsWith("Secret") -or $parameterName.EndsWith("Key")).ToString()
+        $variables | Add-Member -MemberType NoteProperty -Name $parameterName -Value @{"Value" = $_.OriginalValue; "IsSecret" = $isSecret; } -ErrorAction Stop   
+    }     
 }
 
-if ($updateSettings -ne $null) {
-    
-    $variables = New-Object -TypeName psobject
-
-    $global:gl | ForEach-Object {
-        if ($_.AsDeployParameter) {
-            $parameterName = "Deploy." + $_.Name.Replace(":", "_")
-            $isSecret = ($parameterName.EndsWith("Secret") -or $parameterName.EndsWith("Key")).ToString()
-            $variables | Add-Member -MemberType NoteProperty -Name $parameterName -Value @{"Value" = $_.OriginalValue; "IsSecret" = $isSecret; } -ErrorAction Stop   
-        }     
-    }
-    $id = $updateSettings.Id
-    $url = "https://dev.azure.com/$organization/$project/_apis/distributedtask/variablegroups/" + $id + "?api-version=5.0-preview.1"
-    $obj = @{
-        "Type"        = $updateSettings.Type;
-        "Name"        = $updateSettings.Name;
-        "Description" = "Updated"
-        "Variables"   = $variables
-    }
-
-    $body = ($obj | ConvertTo-Json -depth 50).Replace("\u0027", "'")
-    Invoke-RestMethod -Method Put -Uri $url -Headers $autHeaders -ContentType "application/json" -Body $body -ErrorAction Stop
-}
-else {
-    Write-Host "Cannot find appsettings." -ForegroundColor Yellow
-}
-
+UpdateDevOpsLibrary -organization $organization -project $project `
+    -variableGroupName "Deploy" `
+    -variables $variables `
+    -username $username `
+    -password $password
