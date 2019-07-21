0eb60f928855e9db2534d64346a58a57129edcf3
David Lee <seekdavidlee@gmail.com>
2019-07-21 14:27:39 -0500

    Document MyJwtConfig to provide security context.

--- Eklee.Exams.Api/MyJwtConfig.cs
+++ Eklee.Exams.Api/MyJwtConfig.cs
@@ -3,17 +3,27 @@ using Microsoft.Extensions.Configuration;
 
 namespace Eklee.Exams.Api
 {
+	/// <summary>
+	/// This class is used internally to provide authentication and authorization to the Exam API.
+	/// </summary>
 	public class MyJwtConfig : IJwtTokenValidatorParameters
 	{
 		public MyJwtConfig(IConfiguration configuration, IOrganizationsRepository organizationsRepository)
 		{
 			Audience = configuration["Security:Audience"];
 
+			// Organizations represents AAD tenants. The information is stored in the Admin API which is also a GraphQL endpoint.
 			Issuers = organizationsRepository.GetIssuers().GetAwaiter().GetResult();
 		}
 
+		/// <summary>
+		/// Represents the Exam API application.
+		/// </summary>
 		public string Audience { get; }
 
+		/// <summary>
+		/// This represents the federated AAD instances which represents our customers' identity providers.
+		/// </summary>
 		public string[] Issuers { get; }
 	}
 }
